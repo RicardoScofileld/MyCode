2d4f75e6320614df06b251aea56c56c9671599c7
清欢 <drqinghuan@163.com>
2018-03-28 21:02:35 +0800

    实现os.walk方法

--- tornado/hello.py
+++ tornado/hello.py
@@ -13,4 +13,8 @@ if __name__ == '__main__':
         (r'/', IndexHandler),
     ])
     app.listen(8000)
+    # 开启多线程,bind端口，start里面指定开启的进程数量，默认为1，如果为None或者为0，则自动根据机器的cpu生成对应的线程数量，如果大于0，则创建该数量的线程
+    # http_server = tornado.httpserver.HTTPServer(app)
+    # http_server.bind(8000)
+    # http_server.start(0)
     tornado.ioloop.IOLoop.current().start()
